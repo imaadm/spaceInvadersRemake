ad6e491caaab1aba7824c4ba3c3e3f83bc4fd026
Imaad Mohammed <imaadmohammed@gmail.com>
2020-11-07 02:45:36 -0800

    assets

--- PlayerShip.gd
+++ PlayerShip.gd
@@ -20,10 +20,7 @@ func shoot():
 func _process(delta):
 
 	
-	if Input.is_action_pressed("ui_down"):
-		self.position.y += speed*delta
-	if Input.is_action_pressed("ui_up"):
-		self.position.y += -speed*delta
+
 	if Input.is_action_pressed("ui_left"):
 		self.position.x += -speed*delta
 	if Input.is_action_pressed("ui_right"):
